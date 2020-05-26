ffcc6121dbd8acd3f5c703b246ab1fe56fa72014
Iana.Kasimova <Smurfik55>
2020-05-26 22:34:09 +0300

    change POJO

--- src/main/java/com/simply/simplyCookcommon/model/Recipe.java
+++ src/main/java/com/simply/simplyCookcommon/model/Recipe.java
@@ -2,9 +2,14 @@ package com.simply.simplyCookcommon.model;
 
 import java.util.List;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
 
 @Data
+@Document(indexName = "recipe")
 public class Recipe {
+  @Id
+  private String id;
   private String name;
   private long calories;
   private LevelProductComplexity levelProductComplexity;
